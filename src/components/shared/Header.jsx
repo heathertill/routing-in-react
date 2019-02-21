1c537ef05765a02ba4e2cbd3ccbcb515e645eb23
heathertill <heather.till@charter.net>
2019-02-21 13:46:13 -0600

    file structure is working

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import logo from '../assets/logo.png';
+import logo from '../../assets/logo.png';
 
 class Header extends Component {
    render() {
