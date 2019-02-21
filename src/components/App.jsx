85f393cde826f344aea9ffb4c9f7de1dbc422bfa
heathertill <heather.till@charter.net>
2019-02-21 14:55:48 -0600

    created itemCard for display items

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,30 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import Home from './shared/Home';
 import Films from './films/Films';
 import Peeps from './people/Peeps';
-import Header from './shared/Header'
+import Header from './shared/Header';
 
 
-class App extends Component {
-   constructor(props) {
-      super(props); 
-         this.state = {
-            films: [],
-            people: []
-         
-      }
-   }
 
-   componentDidMount() {
-      try {
-         fetch('https://ghibliapi.herokuapp.com/films')
-            .then(resFilms => {
-               return resFilms.json()
-            })
-            .then(films => this.setState(films));
-         fetch('https://ghibliapi.herokuapp.com/people')
-            .then(resPeeps => {
-               return resPeeps.json()
-         })
-            // .then(people => this.setState({ people }))
-      } catch (e) {
-         console.log(e);
-      }
-   }
-   
-
-   render() {
-      return (
-         <Router>
-            <Fragment>
-               <Header />
-               <Link to="/">{<button className="btn btn-primary m-2 d-inline-block">Go Home</button>}</Link>
-               <Link to="/films">View Films</Link>
-               <Link to="/peeps">View People</Link>
-               {/* <Films films={this.state.films} /> */}
-               <Switch>
-                  <Route exact path="/" component={Home}></Route>
-                  <Route path="/films" component={Films} />
-                  <Route path="/peeps" component={Peeps} />
-               </Switch>
-            </Fragment>
-         </Router>
-      );
-   }
-}
+const App = () => {
+   return (
+      <Router>
+         <>
+            <Header />
+            <Link to="/" className="btn btn-primary m-2 d-inline-block">
+               Go Home
+            </Link>
+            <Link to="/films">View Films</Link>
+            <Link to="/peeps">View People</Link>
+            <Switch>
+               <Route exact path="/" component={Home} />
+               <Route path="/films" component={Films} />
+               <Route path="/peeps" component={Peeps} />
+            </Switch>
+         </>
+      </Router>
+   );
+};
 
 export default App;
--- src/components/shared/ItemCard.jsx
+++ src/components/shared/ItemCard.jsx
@@ -1,29 +1,25 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
-const Films = props => {
 
+export default props => {
+   let {
+      id,
+      title,
+   } = props.blog;
    return (
-      <h1>film</h1>
-   )
-   // return props.films.map(film => {
-   //    return (
-   //       <Fragment>
-   //          <h1>This is the films page</h1>
-   //          <div className="card" key={film.id} style={{width: "18rem"}}>
-   //                <div className="card-body">
-   //                   <h5 className="card-title">{film.title}</h5>
-   //                   <p className="card-text">
-   //                      Some quick example text to build on the card title and make
-   //                      up the bulk of the card's content.
-   //                   </p>
-   //                </div>
-   //             </div>
-   //       </Fragment>
-   //    );
-   // })
-   
-      
-   }
-
-
-export default Films;
+      <div className="col-md-4 mb-2">
+         <div className="card" style="width: 18rem;">
+            <img src={logo} className="card-img-top" alt="" />
+            <div className="card-body">
+               <h5 className="card-title">{title}</h5>
+               <Link to={`/film/${id}`} className="btn btn-primary">View Film</Link>
+               <a href="#" className="btn btn-primary">
+                  Go somewhere
+               </a>
+            </div>
+         </div>
+      </div>
+   );
+};
